9713adafe65d4973ab3ec04f34cba9618a23dec6
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2025-03-24 13:22:02 +0000

    Add editing ability

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let budget = 0
-let monthlyCosts = 0
-let annualPaymentsThisMonth = []
-let annualPaymentsNextMonth = []
+let budget,
+    monthlyCosts,
+    annualPaymentsThisMonth,
+    annualPaymentsNextMonth
 
-const WORKER_GET_URL = ""
-const WORKER_UPDATE_URL = ""
+const WORKER_GET_URL = "https://api.alexbowers.co.uk/monthly-costs/get"
+const WORKER_UPDATE_URL = "https://api.alexbowers.co.uk/monthly-costs/update"
+
+const editableCostsWrapper = document.getElementsByClassName("editable-costs")[0]
 
 // Average per month.
 // Some items happen once a year, so the cost is divided by 12.
@@ -20,10 +22,29 @@ fetch(WORKER_GET_URL)
     })
     .catch((err) => showErrorOnPage(err))
 
+const openEditCostsAction = document.getElementById("openEditCostsAction")
+openEditCostsAction.addEventListener("click", function() {
+    editableCostsWrapper.classList.add("show-editable-costs")
+})
+
+const updateCostsAction = document.getElementById("updateCostsAction")
+updateCostsAction.addEventListener("click", function() {
+    postCostsToWorker(
+        JSON.parse(document.getElementById("editableCostsContent").innerText)
+    )
+    closeEditableCostsModal()
+})
+
+const cancelAction = document.getElementById("cancelAction")
+cancelAction.addEventListener("click", function() {
+    closeEditableCostsModal()
+})
+
 function addPaymentsToPage(payments, element) {
     const container = document.getElementById(element)
 
     const list = document.createElement("ul")
+    list.className = "monthly-payment-list"
     payments.forEach((payment) => {
         let listItem = document.createElement("li")
         listItem.innerText = `${formatCamelCaseToWords(payment.name)} (${formatCamelCaseToWords(payment.type)}): ${formatMoney(payment.value * 12)}`
@@ -32,6 +53,10 @@ function addPaymentsToPage(payments, element) {
     container.appendChild(list)
 
     container.classList.add("show-payments")
+}
+
+function closeEditableCostsModal() {
+    editableCostsWrapper.classList.remove("show-editable-costs")
 }
 
 function formatCamelCaseToWords(string) {
@@ -54,9 +79,11 @@ function postCostsToWorker(monthlyCosts) {
 }
 
 async function setAppData(costs) {
+    await setInitialAppData()
     await setBudget(costs.budget)
-    await updateMonthlyCosts(costs.set)
-    await updateMonthlyCosts(costs.variable)
+    setEditableCosts(costs)
+    await storeMonthlyCosts(costs.set)
+    await storeMonthlyCosts(costs.variable)
 
     showCostsOnPage()
     showAnnualPayments()
@@ -65,6 +92,19 @@ async function setAppData(costs) {
 function setBudget(setBudget) {
     budget = setBudget
     document.getElementById("budget").innerText = formatMoney(setBudget)
+}
+
+function setEditableCosts(costs) {
+    document.getElementById("editableCostsContent").innerText = JSON.stringify(costs, null, 2)
+}
+
+function setInitialAppData() {
+    budget = 0
+    monthlyCosts = 0
+    annualPaymentsThisMonth = []
+    annualPaymentsNextMonth = []
+
+    document.querySelectorAll(".monthly-payment-list").forEach(list => list.remove())
 }
 
 function setInitialCosts() {
@@ -94,7 +134,7 @@ function showErrorOnPage(err) {
     document.getElementById("costHeadlines").innerText = err
 }
 
-function updateMonthlyCosts(category) {
+function storeMonthlyCosts(category) {
     let currentMonth = new Date().getMonth() + 1
     category.forEach(type => {
         type.payments.forEach(payment => {
